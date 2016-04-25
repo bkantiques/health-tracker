d117ff56ad4ddc36a1f43035c8336a1bc3b997bf
JReed <jreed0719@gmail.com>
2016-04-25 19:26:53 -0400

    Add features: Indicate on calendar if a day has records and display total calories for a day

--- src/js/collections/foodRecords.js
+++ src/js/collections/foodRecords.js
@@ -8,6 +8,60 @@ var FoodRecords = Backbone.Firebase.Collection.extend({
 
 	url: function() {
 		return 'https://torrid-fire-530.firebaseio.com/users/' + app.User.get('authData').uid
+	},
+
+	// Get records from given date
+	getRecordsByDate: function(date) {
+
+		// Check that date is valid
+		if(date && !isNaN(date.getTime())) {
+
+			// Function to tell if the date of a record matches the given date
+			function matchesSelectedDate(recordItem) {
+				var recordDate = new Date(recordItem.get('date'));
+
+				return recordDate.getFullYear() === date.getFullYear() &&
+				recordDate.getMonth() === date.getMonth() &&
+				recordDate.getDate() === date.getDate();
+			};
+
+			return this.filter(matchesSelectedDate);
+
+		}
+
+	},
+
+	// Get calories consumed on a given date
+	getCaloriesOnDate: function(date) {
+
+		var totalCalories = 0;
+
+		// Check that date is valid
+		if(date && !isNaN(date.getTime())) {
+
+			var records = this.getRecordsByDate(date);
+
+			records.forEach(function(record) {
+				var recordCalories = record.get('calories');
+
+				// If recordCalories is a number, add to tota calories
+				if(!isNaN(recordCalories)) {
+					totalCalories += record.get('calories');
+				}
+			});
+
+		}
+
+		return totalCalories;
+	},
+
+	// Get array of dates objects with records on those dates
+	getDates: function() {
+		// Get array of date json strings from records without repetitions, then turn each into a date object
+		return _.union(this.pluck('date'))
+			.map(function(dateString) {
+				return new Date(dateString);
+			});
 	}
 
 });
--- src/js/views/record.js
+++ src/js/views/record.js
@@ -1,5 +1,15 @@
 var app = app || {};
 
+
+var dateUtility = {
+	// Return whether 2 dates have the same year, month and day, i.e. are on the same day
+	sameDay: function sameDay(date1, date2) {
+			return date1.getFullYear() === date2.getFullYear() &&
+			date1.getMonth() === date2.getMonth() &&
+			date1.getDate() === date2.getDate();
+		}
+};
+
 RecordView = Backbone.View.extend({
 
 	el: $('#record-view'),
@@ -11,6 +21,7 @@ RecordView = Backbone.View.extend({
 	initialize: function() {
 		this.$recordList = this.$('#record-list');
 		this.$datepicker = this.$('#datepicker');
+		this.$dayTotalCalories = this.$('#day-total-calories');
 
 		// Initialize datepicker, set to today's date to start
 		this.$datepicker.datepicker();
@@ -24,39 +35,60 @@ RecordView = Backbone.View.extend({
 
 	},
 
-	// Add a search item view for each search item in the collection
+	// Add a record item view for each record item in the collection
+	// and display total calories for the day
 	renderList: function() {
 		var self = this;
 
 		// Clear old record items and error message
 		this.$recordList.html('');
+		this.$dayTotalCalories.text('');
 
 		var date = this.getDate();
 
 		// Check if date from datepicker is valid and collection exists
 		if(app.FoodRecords && date && !isNaN(date.getTime())) {
 
-			// Function to tell if the date of a record matches the datepicker date
-			function matchesSelectedDate(recordItem) {
-				var recordDate = new Date(recordItem.get('date'));
+			var records = app.FoodRecords.getRecordsByDate(date);
 
-				return recordDate.getFullYear() === date.getFullYear() &&
-				recordDate.getMonth() === date.getMonth() &&
-				recordDate.getDay() === date.getDay();
-			};
-
-			app.FoodRecords.filter(matchesSelectedDate).forEach(function(recordItem) {
+			records.forEach(function(recordItem) {
 
 				var view = new app.RecordItemView({model: recordItem});
 				self.$recordList.append(view.render().el);
 
 			});
 
+			this.$dayTotalCalories.text('Total calories: ' + app.FoodRecords.getCaloriesOnDate(date));
+
 		}
 
 	},
 
 	renderCalendar: function() {
+		// Get array of date strings from records without repetitions, then turn each into a date object
+		var dates = app.FoodRecords.getDates();
+
+		function beforeShowDay(calendarDate) {
+			// Find index of date in dates that is on same day as calendar date
+			var index = _.findIndex(dates, function(recordDate) {
+				return dateUtility.sameDay(calendarDate, recordDate);
+			});
+
+			// If index not found, don't add a css class to that calendar date and add a tooltip saying
+			// no recorded food
+			if(index === -1) {
+				return [true, '', 'No recorded food on this date'];
+			}
+			// If index is found, add a css class and add a tooltip saying there is recorded food
+			else {
+				return [true, 'has-record', 'You recorded food on this date'];
+			}
+		}
+
+		// Refresh datepicker against new set of dates
+		this.$datepicker.datepicker('option', 'beforeShowDay', beforeShowDay);
+
+		this.$datepicker.datepicker('refresh');
 
 	}
 
